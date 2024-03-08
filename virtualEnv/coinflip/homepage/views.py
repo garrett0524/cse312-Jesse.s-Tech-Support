cd394a0ef0fb44cf56be74fc10fbfc275dd5d987
Christopher Pena <cpena3@buffalo.edu>
2024-03-08 01:01:45 -0500

    Added starterHTML, which you can work on to create the actual homepage HTML

--- virtualEnv/coinflip/homepage/views.py
+++ virtualEnv/coinflip/homepage/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 # Create your views here.
 
-def homepage(request):
-    return HttpResponse("Welcome to Coinflip!")
+def homepage(request):  
+    template = loader.get_template("starterHTML.html")
+    return HttpResponse(template.render())
 
 
