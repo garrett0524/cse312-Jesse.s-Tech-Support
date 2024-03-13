1b836e5d12f35d4c34a21d3dfd8513e26d2c4168
Christopher Pena <cpena3@buffalo.edu>
2024-03-13 09:16:46 -0400

    Created register and login pages. Buttons on homepage redirect you respectively

--- virtualEnv/coinflip/homepage/urls.py
+++ virtualEnv/coinflip/homepage/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.homepage, name='members'),
+    path('', views.homepage, name='homepage'),
+    path('register', views.register, name='register'),
+    path('login', views.login,name='login'),
+
 ]
--- virtualEnv/coinflip/homepage/views.py
+++ virtualEnv/coinflip/homepage/views.py
@@ -8,4 +8,12 @@ def homepage(request):
     template = loader.get_template("starterHTML.html")
     return HttpResponse(template.render())
 
+def register(request):
+    template = loader.get_template("register.html")
+    return HttpResponse(template.render())
+
+def login(request):
+    template = loader.get_template("login.html")
+    return HttpResponse(template.render())
+
 
