67fe5bb53d3237ac992508419654aa50aa8dfc1a
Christopher Pena <cpena3@buffalo.edu>
2024-03-24 22:49:15 -0400

    SQL Connected?

--- virtualEnv/coinflip/coinflip/settings.py
+++ virtualEnv/coinflip/coinflip/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -75,11 +77,13 @@ WSGI_APPLICATION = 'coinflip.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL'),
+        conn_max_age=600,
+        #ssl_require=True
+    )
 }
+
 
 
 # Password validation
