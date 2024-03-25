c033fad8014d7f75c56156a9021e936470e79d68
dinulawe <Nethan.w12@gmail.com>
2024-03-24 22:53:17 -0400

    messy implemenatation

--- virtualEnv/coinflip/coinflip/settings.py
+++ virtualEnv/coinflip/coinflip/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         'NAME': 'coinflip',
         'USER': 'root',
         'PASSWORD': 'password',
-        'HOST': 'db',
+        'HOST': '127.0.0.1 ',
         'PORT': '3306',
     }
 }
