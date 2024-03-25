a85fb096f2f45b4ec878973b073840b71841e1aa
dinulawe <99000000+dinulawe@users.noreply.github.com>
2024-03-25 04:11:32 -0400

    chat_data model

--- virtualEnv/coinflip/homepage/models.py
+++ virtualEnv/coinflip/homepage/models.py
@@ -4,3 +4,7 @@ from django.contrib.auth.models import User
 class AuthToken(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     token = models.CharField(max_length=255)
+
+class Chat_Data(models.Model):
+    user = models.CharField(max_length = 50)
+    message = models.CharField(max_length = 200)
