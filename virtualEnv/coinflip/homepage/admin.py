998c773b042618921f489493a34dbd0e4a49d5ad
Christopher Pena <cpena3@buffalo.edu>
2024-04-10 21:23:07 -0400

    Added new model for user profile, added register field requirements.

--- virtualEnv/coinflip/homepage/admin.py
+++ virtualEnv/coinflip/homepage/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Chat_Data
+from .models import Chat_Data, UserProfile
 
 
 # Register your models here.
 
 admin.site.register(Chat_Data)
+admin.site.register(UserProfile)
