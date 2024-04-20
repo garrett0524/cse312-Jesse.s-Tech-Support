a5a3e7b1a70cf5773c9360ee34b3e99881dcc7d7
Christopher Pena <cpena3@buffalo.edu>
2024-04-19 22:11:30 -0400

    Coinflip game functionality implemented.

--- virtualEnv/coinflip/homepage/admin.py
+++ virtualEnv/coinflip/homepage/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from .models import Chat_Data, UserProfile
+from .models import Chat_Data, UserProfile, Player, Game
 
 
 # Register your models here.
 
 admin.site.register(Chat_Data)
 admin.site.register(UserProfile)
+admin.site.register(Player)
+admin.site.register(Game)
