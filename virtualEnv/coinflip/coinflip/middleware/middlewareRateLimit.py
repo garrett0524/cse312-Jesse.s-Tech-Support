3b8f0b6daff4c9500c1b3639d683365ece51be7c
Christopher Pena <cpena3@buffalo.edu>
2024-05-06 03:09:43 -0400

    Update

--- virtualEnv/coinflip/coinflip/middleware/middlewareRateLimit.py
+++ virtualEnv/coinflip/coinflip/middleware/middlewareRateLimit.py
@@ -11,7 +11,7 @@ class RateLimitMiddleware:
 
     def __call__(self, request):
         ip_address = request.META.get('REMOTE_ADDR')
-        #logger.info(f"Incoming request from IP: {ip_address}")
+        logger.info(f"Incoming request from IP: {ip_address}")
 
         request_count_key = f'request_count_{ip_address}'
         block_expiry_key = f'block_expiry_{ip_address}'
@@ -19,32 +19,36 @@ class RateLimitMiddleware:
         current_time = int(time.time())
         time_window = 10  # Time window in seconds
 
+        # Get the timestamps of the previous requests within the time window
         timestamps = cache.get(request_count_key, [])
-        #logger.info(f"Previous timestamps: {timestamps}")
+        logger.info(f"Previous timestamps: {timestamps}")
 
+        # Remove timestamps older than the time window
         timestamps = [t for t in timestamps if t >= current_time - time_window]
-        #logger.info(f"Filtered timestamps: {timestamps}")
+        logger.info(f"Filtered timestamps: {timestamps}")
 
-        #timestamps.append(current_time)
+        # Add the current timestamp to the list
+        timestamps.append(current_time)
 
         # Count the number of requests within the time window
         request_count = len(timestamps)
-        #logger.info(f"Request count: {request_count}")
+        logger.info(f"Request count: {request_count}")
 
+        # Set the updated timestamps in the cache
         cache.set(request_count_key, timestamps, timeout=time_window)
 
         block_expiry = cache.get(block_expiry_key)
-        #logger.info(f"Block expiry: {block_expiry}")
+        logger.info(f"Block expiry: {block_expiry}")
 
         if block_expiry and current_time < block_expiry:
-            #logger.warning(f"IP {ip_address} is blocked. Returning 429 response.")
+            logger.warning(f"IP {ip_address} is blocked. Returning 429 response.")
             return HttpResponse("Too Many Requests. Please try again later.", status=429)
 
         if request_count > 50:
-            #logger.warning(f"IP {ip_address} has exceeded the rate limit. Blocking for 30 seconds.")
+            logger.warning(f"IP {ip_address} has exceeded the rate limit. Blocking for 30 seconds.")
             cache.set(block_expiry_key, current_time + 30)
             return HttpResponse("Too Many Requests. Please try again after 30 seconds.", status=429)
 
-        #logger.info(f"IP {ip_address} is allowed to proceed.")
+        logger.info(f"IP {ip_address} is allowed to proceed.")
         response = self.get_response(request)
         return response
